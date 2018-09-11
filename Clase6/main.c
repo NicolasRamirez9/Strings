0112eee1a878d0f8f8f6403460f465db204c8b90
NicolasRamirez9 <nicocai98@gmail.com>
2018-09-11 09:38:57 -0300

    Arrays.

--- Clase6/main.c
+++ Clase6/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main()
 {
@@ -8,6 +9,7 @@ int main()
     char apellido[20];
     char nombreCompleto[41] = "";
     char buffer[1000];
+    int i;
 
     printf("Ingrese nombre: ");
     fflush(stdin);
@@ -34,6 +36,18 @@ int main()
     strcat(nombreCompleto , apellido);
     strcat(nombreCompleto , ", ");
     strcat(nombreCompleto , nombre);
+
+    strlwr(nombreCompleto);
+
+    nombreCompleto[0] = toupper(nombreCompleto[0]);
+
+    for(i = 0; i < strlen(nombreCompleto); i++)
+    {
+        if(nombreCompleto[i] == ' ')
+        {
+            nombreCompleto[i + 1] = toupper(nombreCompleto[i + 1]);
+        }
+    }
 
     puts(nombreCompleto);
 
