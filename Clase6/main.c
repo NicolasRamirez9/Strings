b2e9bf73d61bde2e516730f1045168f66a3dbcde
NicolasRamirez9 <nicocai98@gmail.com>
2018-09-11 09:22:33 -0300

    Arrays.

--- Clase6/main.c
+++ Clase6/main.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
     char nombre[20];
     char apellido[20];
-    char nombreCompleto[41];
+    char nombreCompleto[41] = "";
+    char buffer[1000];
 
     printf("Ingrese nombre: ");
-    fflug(stdin);
-    gets(nombre);
+    fflush(stdin);
+    gets(buffer);
+    while(strlen(buffer) > 19)
+    {
+        printf("Re-ingrese nombre: ");
+        fflush(stdin);
+        gets(buffer);
+    }
+    strcpy(nombre , buffer);
+
+    printf("Ingrese apellido: ");
+    fflush(stdin);
+    gets(buffer);
+    while(strlen(buffer) > 19)
+    {
+        printf("Re-ingrese apellido: ");
+        fflush(stdin);
+        gets(buffer);
+    }
+    strcpy(apellido , buffer);
+
+    strcat(nombreCompleto , apellido);
+    strcat(nombreCompleto , ", ");
+    strcat(nombreCompleto , nombre);
+
+    puts(nombreCompleto);
 
     return 0;
 }
