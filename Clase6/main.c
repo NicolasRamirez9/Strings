df14a6c3ffb897234e5f264b6e699ab9e75fe14a
NicolasRamirez9 <nicocai98@gmail.com>
2018-09-11 09:07:29 -0300

    Arrays.

---
+++ Clase6/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char nombre[20];
+    char apellido[20];
+    char nombreCompleto[41];
+
+    printf("Ingrese nombre: ");
+    fflug(stdin);
+    gets(nombre);
+
+    return 0;
+}
